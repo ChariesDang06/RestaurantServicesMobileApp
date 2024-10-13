e672a82893785ac99e3ddefe50b8840bcd1e59de
ChariesDang <chariesdang06@gmail.com>
2024-10-13 09:58:31 +0700

    add navbar & banner-promo components

--- src/app/components/banner-promo/banner-promo.component.ts
+++ src/app/components/banner-promo/banner-promo.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+register()
 
 @Component({
+  standalone: true,
   selector: 'app-banner-promo',
   templateUrl: './banner-promo.component.html',
   styleUrls: ['./banner-promo.component.scss'],
+  imports: [IonicModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class BannerPromoComponent  implements OnInit {
-
+  promos = [
+    { promoHeader: 'Nhận ngay', promoAmount:'10% Off', promoContent:'cho cái bơ gơ bự đùng', src: 'assets/images/image1.png', alt: 'Image 1' },
+    { promoHeader: 'Nhận ngay', promoAmount:'20% Off', promoContent:'cho cái bơ gơ bự đùng', src: 'assets/images/image2.png', alt: 'Image 2' },
+    { promoHeader: 'Nhận ngay', promoAmount:'30% Off', promoContent:'cho cái bơ gơ bự đùng', src: 'assets/images/image3.png', alt: 'Image 3' },
+  ];
   constructor() { }
 
   ngOnInit() {}
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { IonicModule } from '@ionic/angular';
+// import { AuthService } from 'src/app/services/auth.service';
+// import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss'],
-})
-export class NavbarComponent  implements OnInit {
+// @Component({
+//   standalone: true,
+//   selector: 'app-navbar',
+//   templateUrl: './navbar.component.html',
+//   styleUrls: ['./navbar.component.scss'],
+//   imports: [IonicModule, CommonModule]
+// })
+// export class NavbarComponent  implements OnInit {
 
-  constructor() { }
+//   isLoggedIn: boolean = false;
+//   constructor(private authService: AuthService) {}
+  
+//   ngOnInit() {
+//     this.authService.isLoggedIn$.subscribe(status => {
+//       this.isLoggedIn = status;
+//     });
+//   }
+//   login() {
+//     this.authService.login();
+//   }
 
-  ngOnInit() {}
-
-}
+//   logout() {
+//     this.authService.logout();
+//   }
+  
+// }
